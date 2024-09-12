82fdfed55e30b91e135f61df843a12fee0005002
Nic Ballesteros <nicballesteros2@gmail.com>
2024-09-12 13:57:03 -0400

    Fix QR showing

--- src/components/Writer.vue
+++ src/components/Writer.vue
@@ -2,9 +2,14 @@
   <div>
     <button @click="showQR = !showQR" class="border-1 border border-black rounded p-1"
       >QR</button>
-    <div class="fixed" v-if="showQR">
-      <VueQRcode :value="data" :color="{ dark: '#000'}" type="image/png"/>
-    </div>
+    <Teleport to="body">
+      <div class="fixed top-0 right-0 w-screen h-screen bg-black" v-if="showQR">
+        <div class="flex flex-col items-center gap-2">
+          <VueQRcode :value="data" :color="{ dark: '#000'}" type="image/png" class="w-1/3"/>
+          <button class="bg-white text-black py-1 px-2 rounded" @click="() => showQR = false">Close</button>
+        </div>
+      </div>
+    </Teleport>
   </div>
 </template>
 
